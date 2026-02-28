4371182cecfa8c5c6508105fed6284497aa22652
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-03-01 01:03:45 +0530

    File system Operations Completed read write delete and create

--- Files.js
+++ Files.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const { log } = require('console');
+const fs = require('fs'); // * all are asynchronous tasks 
 //! Reading files 
 
 // fs.readFile('./Docs/Blog1.txt' , (err , data) => {
@@ -21,17 +22,49 @@ const fs = require('fs');
 
 //! Writing files 
 
-fs.writeFile('./Docs/Blog1.txt' , 'Hello , world' , () => {
-    console.log(`edited the file`);
-})
-// create a file 
-fs.writeFile('./Docs/Blog2.txt' , 'Hello , again' , () => {
-    console.log(`edited the file`);
-})
-
-
+// fs.writeFile('./Docs/Blog1.txt' , 'Hello , world' , () => {
+//     console.log(`edited the file`);
+// })
+// ! create a file 
+// fs.writeFile('./Docs/Blog2.txt' , 'Hello , again' , () => {
+//     console.log(`edited the file`);
+// })
 
 
 
 //! Directories
+if(!fs.existsSync('./assets')) {
+fs.mkdir('./assets' , (err) => {
+    if(err){
+        console.log(err);
+    }
+    console.log(`created the directory`)
+})
+}
+// ! removed the directory 
+else{
+    fs.rmdir('./assets' , (err) => {
+    if(err){
+        console.log(err);
+    }
+    console.log(`removed the directory`)
+
+})
+}
+
+
 //! Deleting files! 
+if(fs.existsSync('./Docs/deleteme.txt')){
+    fs.unlink('./Docs/deleteme.txt' , (err) => {
+        if(err){
+            console.log(err);
+            
+        }
+        console.log(`file was deleted successfully`)
+    })
+}
+else{
+    fs.writeFile('./Docs/deleteme.txt' , 'hello again' , () => {
+        console.log(`file was created successfully`)
+    })
+}
