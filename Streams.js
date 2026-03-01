726f89bf2a9dcdfad91038bb9848d58ce6155ded
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-03-01 15:28:01 +0530

    readStream & writeStream done piping also completed

--- Streams.js
+++ Streams.js
@@ -2,7 +2,15 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream('./Docs/blog3.txt', {encoding : 'utf8'});
 
-readStream.on('data' , (chunk)=>{
-    console.log(`--new chunk--`);
-    console.log(chunk);
-})
+const writeStream = fs.createWriteStream('./Docs/blog4.txt', {encoding : 'utf8'});
+
+// readStream.on('data' , (chunk)=>{
+//     console.log(`--new chunk--`);
+//     console.log(chunk);
+//     writeStream.write(`\n Next chunk is Processed\n`);
+//     writeStream.write(chunk);
+// })
+
+// a much shoerted way to do this is piping
+
+readStream.pipe(writeStream);
